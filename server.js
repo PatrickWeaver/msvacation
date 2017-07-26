4234fe6d63096865946cf2f7b1ede6cb711fd709
System <none>
2017-07-26 21:45:04 +0000

    Eliminate unnecessary files, get status from status.json

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var request = require('request')
+var jsonfile = require('jsonfile');
+
+
+var status;
+
 
 
 var app = express();
@@ -15,102 +20,40 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 
-
-var db;
-
-if(process.env.ENV == 'Test'){
-  db = mongoose.connect('mongodb://localhost/msvacation_test');
-}
-else {
-  db = mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/msvacation');
-}
-
-var Vacation = require('./models/vacationModel');
-
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
-
-var vacationRouter = require('./routes/vacationRoutes')(Vacation);
-
-
-app.use('/api/vacations', vacationRouter);
 
 var apidata;
 var duration;
 var onvacation;
 
 
-var durationlog = console.log(duration + " <<<")
-
-var vacationfind = function(callback) {
-  Vacation.find({ place: "Palos Verdes"}, function(err, vacation) {
-  if (err) throw err;
-
-  console.log(vacation);
-  apidata = vacation;
-  duration = vacation.duration;
-});
-};
-
-vacationfind(durationlog);
-
-console.log(apidata);
-
-console.log(duration + " <<");
-
-
-date = new Date();
-todayOfTheWeek = date.getDay();
-
-
-/*app.get('/toggle', function(req, res) {
-
-  if( onvacation ) {
-    onvacation = false
-  } else {
-    onvacation = true
-  }
-
-  res.render('thankyou', {
-    css: 'toggle'
-  });
-
-});*/
+var date = new Date();
+var todayOfTheWeek = date.getDay();
 
 
 app.get('/', function(req, res){
-  /*if (true) {
-  //if (onvacation == true) {
-    res.render('index', {
-      place: '',
-      duration: duration,
-      transportation: 'test',
-      css: 'style'
-    });
-  //}
-  */
-  /*  else if (todayOfTheWeek > 5) {
-    res.render('weekend', {
-      place: '',
-      duration: duration,
-      transportation: 'test',
-      css: 'weekend'
-    });
-
-  } else {
-  */
+  jsonfile.readFile(__dirname + "/status.json", function(err, obj) {
+    console.log(obj);
+    status = obj.status;
+    
+  if (status === "working") {
     res.render('vacationindex', {
       place: '',
       duration: duration,
       transportation: '',
       css: 'vacation'
     });
-  //};
-});
+    
+  }
+    
+    
+  })
+  
 
+});
 
 
 app.use('/public', express.static(__dirname + '/public'));
